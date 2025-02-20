a9f90e7e4c3a2beb22c7bab2a932ce2cb5c39f47
rawanatef634 <atron944@gmail.com>
2025-02-20 23:47:05 +0200

    updated images

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -1,22 +1,22 @@
 import { atom, useAtom } from "jotai";
 
 const pictures = [
-  "img1",
-  "img2",
-  "img3",
-  "img4",
-  "img17",
-  "img6",
-  "img9",
-  "img8",
-  "img7",
-  "img12",
-  "img10",
-  "img11",
-  "img16",
-  "img14",
-  "img15",
-  "img18",
+  "DSC00680",
+  "DSC00933",
+  "DSC00966",
+  "DSC00983",
+  "DSC01011",
+  "DSC01040",
+  "DSC01064",
+  "DSC01071",
+  "DSC01103",
+  "DSC01145",
+  "DSC01420",
+  "DSC01461",
+  "DSC01489",
+  "DSC02031",
+  "DSC02064",
+  "DSC02069",
 ];
 
 export const pageAtom = atom(0);
